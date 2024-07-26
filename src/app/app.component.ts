48fc4e3e3e9e164fdf405cd4274e086be770c3f0
Sebastian Rieder <130756424+SebastianClausRieder@users.noreply.github.com>
2024-07-26 13:16:15 +0200

    generate Login and SignUp Area

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MasterLoginSignUpComponent } from './master-login-sign-up/master-login-sign-up.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MasterLoginSignUpComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/master-login-sign-up/master-login-sign-up.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-master-login-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './master-login-sign-up.component.html',
+  styleUrl: './master-login-sign-up.component.scss'
+})
+export class MasterLoginSignUpComponent {
+
+}
