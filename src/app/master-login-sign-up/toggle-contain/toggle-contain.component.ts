97fbed0723d7394c595a9a4bdde836dc2660adfb
Sebastian Rieder <130756424+SebastianClausRieder@users.noreply.github.com>
2024-07-27 16:21:52 +0200

    worke on login

--- src/app/master-login-sign-up/toggle-contain/toggle-contain.component.ts
+++ src/app/master-login-sign-up/toggle-contain/toggle-contain.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { GlobalJSService } from '../../global-js.service';
 
 @Component({
   selector: 'app-toggle-contain',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './toggle-contain.component.html',
   styleUrl: './toggle-contain.component.scss'
 })
 export class ToggleContainComponent {
+
+  globalJSData = inject(GlobalJSService);
 
 }
