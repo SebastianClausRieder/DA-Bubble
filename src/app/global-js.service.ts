b44b64c8b75d8bcd266b5423b6612c09a4ac241b
Sebastian Rieder <130756424+SebastianClausRieder@users.noreply.github.com>
2024-07-28 16:13:05 +0200

    worke on login

--- src/app/global-js.service.ts
+++ src/app/global-js.service.ts
@@ -8,7 +8,7 @@ export class GlobalJSService {
   private signUpSubject = new BehaviorSubject<boolean>(false);
   signUp$ = this.signUpSubject.asObservable();
 
-  isGerman: boolean = true;
+  isGerman: boolean = false;
 
   toggleSignUp() {
     this.signUpSubject.next(!this.signUpSubject.value);
