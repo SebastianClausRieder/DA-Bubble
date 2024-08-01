ef9aeeea96cc53f573e7aa30ed9408fdc6421b9e
hanbit10 <chanbit10@gmail.com>
2024-08-01 15:15:48 +0200

    update components

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -4,16 +4,28 @@ import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [RouterModule, MatIconModule, MatDividerModule, MatButtonModule],
+  imports: [
+    RouterModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    HeaderComponent,
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
   firestore: Firestore = inject(Firestore);
+  showFiller = false;
   constructor(private router: Router) {}
 
   async saveUser() {
