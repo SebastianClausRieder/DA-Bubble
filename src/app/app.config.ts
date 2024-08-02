1e2b23e252bce4d83fb1dff35968e7a6e37c83f8
Sebastian Rieder <130756424+SebastianClausRieder@users.noreply.github.com>
2024-08-02 15:12:19 +0200

    worke on login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -22,6 +22,6 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     provideFirestore(() => getFirestore()),
-    provideAnimationsAsync(),
+    provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(),
   ],
 };
